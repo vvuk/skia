77d5129e95ecb5f32f7224bd68831624345e3691
Vladimir Vukicevic <vladimir@pobox.com>
2016-11-28 20:25:56 -0500

    fix.. a thing? GLFW_TRUE?

--- cmake/viewer.cpp
+++ cmake/viewer.cpp
@@ -94,7 +94,7 @@ static void error_callback(int error, const char* description)
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
 
